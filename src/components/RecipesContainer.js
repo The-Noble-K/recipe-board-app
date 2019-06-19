e0ad1b38199a6843921f33800864b4fa5e01dc05
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-19 19:06:39 -0400

    added RecipesContainer

---
+++ src/components/RecipesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class RecipesContainer extends Component {
+    render() {
+        return(
+            <div>
+                Recipes
+            </div>
+        )
+    }
+}
+
+export default RecipesContainer
