5c7db80c744635f82eb11515509ef33fcce5b2b1
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-20 02:00:36 -0400

    display recipes, styling

--- src/components/RecipesContainer.js
+++ src/components/RecipesContainer.js
@@ -22,9 +22,17 @@ class RecipesContainer extends Component {
     render() {
         return(
             <div>
-                Recipes
+                {this.state.recipes.map((recipe) => {
+                    return(
+                        <div className='tile' key={recipe.id} >
+                            <h3>{recipe.title}</h3>
+                            <p>{recipe.ingredients}</p>
+                            <p>{recipe.instructions}</p>
+                        </div>
+                    )
+                })}
             </div>
-        )
+        );
     }
 }
 
